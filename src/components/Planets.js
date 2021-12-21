817ddc9ca03b4c4c846db8ccff93c2e005e854f8
nairoMudumane <nazirmudumane@gmail.com>
2021-12-21 08:55:50 +0200

    bug: queryClientProvider

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Planet from './Planet';
 
-export default function Planets() {
-  return (
-    <div>
-      <h2>planets</h2>
-    </div>
-  );
-}
+const fetchPlanets = async () => {
+  const res = await fetch('http://swapi.dev/api/planets');
+  return res.json();
+};
+
+const Planets = () => {
+  const { data, isLoading, error } = useQuery('planets', fetchPlanets);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error</p>;
+  }
+
+  console.log(data);
+  return <p>success</p>;
+};
+export default Planets;
